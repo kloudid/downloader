84516874867492afeae06751f16ed36209bc9179
kloudid <calm.pen1928@fastmail.com>
2023-05-21 19:51:54 -0500

    Added video or audio options. Added basic css (it's ugly).

--- app.py
+++ app.py
@@ -6,11 +6,17 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'videos'
 
 
-def download_video(url):
+def download_video(url, download_video=True, download_audio=False):
     ydl_opts = {
-        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio',
         'outtmpl': f"{app.config['UPLOAD_FOLDER']}/%(title)s.%(ext)s",
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'merge_output_format': 'mp4',
+        'writeautomaticsub': True,
     }
+    if not download_video:
+        ydl_opts['writesubtitles'] = True
+        ydl_opts['postprocessors'] = [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192'}]
+
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
@@ -23,8 +29,18 @@ def index():
 @app.route('/download', methods=['POST'])
 def download():
     url = request.form['url']
+    download_video_option = request.form.get('download_video')
+    download_audio_option = request.form.get('download_audio')
     try:
-        download_video(url)
+        if download_video_option == 'on' and download_audio_option == 'on':
+            download_video(url, download_video=True, download_audio=False)
+        elif download_video_option == 'on':
+            download_video(url, download_video=True, download_audio=True)
+        elif download_audio_option == 'on':
+            download_video(url, download_video=False, download_audio=True)
+        else:
+            return render_template('error.html')
+        
         video_filenames = os.listdir(app.config['UPLOAD_FOLDER'])
         video_filename = video_filenames[-1]  # Get the latest video file
         return render_template('success.html', video_filename=video_filename)
@@ -32,7 +48,7 @@ def download():
         return render_template('error.html')
 
 
-@app.route('/uploads/<path:filename>')
+@app.route('/videos/<path:filename>')
 def serve_static(filename):
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
 
